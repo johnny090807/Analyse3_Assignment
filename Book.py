bab75a42b6496eaca3077034a49eaba15868119d
johnny090807 <33625576+johnny090807@users.noreply.github.com>
2020-04-02 13:40:05 +0200

    book toegevoegd

---
+++ Book.py
@@ -0,0 +1,6 @@
+class Book:
+    def __init__(self, naam):
+        self._naam = naam
+
+    def getNaam(self):
+        return self._naam
---
+++ main.py
@@ -0,0 +1,5 @@
+from Book import Book
+
+boek1 = Book("Knibbel")
+
+print(boek1.getNaam())
