ee27f5a62e9c39e2d898bcd45f4a15efb5099b27
RBLeon <57948143+RBLeon@users.noreply.github.com>
2020-04-02 16:01:51 +0200

    Added separate file for reading and writing

--- main.py
+++ main.py
@@ -8,26 +8,3 @@ print("__Reserverings Systeem__")
 print("__typ 1 om in te loggen__")
 print("__typ 2 om in te loggen__")
 print("__typ 3 om in te loggen__")
-
-#Write
-
-with open('employee_file2.csv', mode='w') as csv_file:
-    fieldnames = ['emp_name', 'dept', 'birth_month']
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
-    writer.writeheader()
-    writer.writerow({'emp_name': 'John Smith', 'dept': 'Accounting', 'birth_month': 'November'})
-    writer.writerow({'emp_name': 'Erica Meyers', 'dept': 'IT', 'birth_month': 'March'})
-
-#Read
-
-with open('employee_birthday.txt', mode='r') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        print(f'\t{row["name"]} works in the {row["department"]} department, and was born in {row["birthday month"]}.')
-        line_count += 1
-    print(f'Processed {line_count} lines.')
