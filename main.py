1c11168994629fd9305eba737b3350afa40c550f
Dominique-Mahu <58041471+Dominique-Mahu@users.noreply.github.com>
2020-04-02 16:53:05 +0200

    design

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
 from Book import Book
 
 
-boek1 = Book("Knibbel", "bla bla", 123)
+boek1 = Book("Knibbel", "bla bla", 123, 213)
 
 
 print("__Reserverings Systeem__")
-print("__typ 1 om in te loggen__")
-print("__typ 2 om in te loggen__")
-print("__typ 3 om in te loggen__")
+user = input("Subscriber press 1 | Librarian fill in password")
+
+while user == "1":
+    print("Welcome Subscriber!")
+    bookSearch = input("Search for a book by title, author or ISBN \nif you want to cancel enter QUIT ")
+    if bookSearch == "":
+        print("BOOK INFO, AVAILABILITY")
+        availability = True
+        if availability:
+            purchase = input("press 1 to loan the book \npress 2 to search for another book")
+            if purchase == "1":
+                print("congratulations you loaned the book ")
+    elif bookSearch == "QUIT":
+        user = "0"
+
+while user == "password123":
+    print("Welcome Librarian!")
+    librarianAction = input("press 1 to add a book \npress 2 to add a customer "
+                            "\npress 3 to make a back up \nto quit enter QUIT")
+    if librarianAction == "1":
+        input("details of the new book item")
+    elif librarianAction == "2":
+        input("details of the new customer")
+    elif librarianAction == "3":
+        input("make a back up")
+    elif librarianAction == "QUIT":
+        user = "0"
+
+
