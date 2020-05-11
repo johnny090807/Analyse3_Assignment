36c55e1ad1282a422091adeb7d8d8b8f159d0943
RBLeon <57948143+RBLeon@users.noreply.github.com>
2020-05-11 20:38:47 +0200

    temporarily push for john

--- loanAdministration.py
+++ loanAdministration.py
@@ -1,3 +1,4 @@
+import csv
 from Person import Person
 from Catalog import Catalog
 from Book import Book
@@ -10,23 +11,36 @@ class LoanAdministration:
         self.__persoonId = persoonId
         self.__loanId = loanId
 
+
     def getPersoonId(self):
         return self.__persoonId
 
     def getLoanId(self):
         return self.__loanId
+    
+
+    
+    def reserveer_boek(self, loggedinUser, persoonId, loanId):
+        print("***** Reserveer een boek *****")
+        titel_boek = input("Zoek boek op titel of ISBN:")
+        for i in books:
+            if titel_boek == i.getTitle or titel_boek == i.getISBN:
+                print(i.printBook)
+                reserveren_boek = input("1) Reserveer het boek \n0) Reserveer het boek niet")
+                if reserveren_boek == "1":
+                    #Add the book to Person localy
+                    loggedinUser.addBookToLoaned(i.getLoanId)
+                    #Add the book to Person in csv 
+                    fields=[persoonId,loanId]
+                    with open(r'loanAdministration.csv', 'a') as addLoan:
+                        writer = csv.writer(addLoan)
+                        writer.writerow(fields)
+                    print("Het boek is toegevoegd aan de geleende boeken")
+                else:
+                   start_keuze_gebruiker = input("6) Zoek opnieuw \n0) Terug naar menu") 
+            else:
+                print("titel bestaat niet")
+                start_keuze_gebruiker = input("6) Zoek opnieuw \n0) Terug naar menu")
 
 books = catalog.returnBooks()
 
-def reserveer_boek():
-    print("***** Reserveer een boek *****")
-    titel_boek = input("Zoek boek op titel of ISBN:")
-    for i in books:
-        if titel_boek == i.getTitle or titel_boek == i.getISBN:
-            print(i.printBook)
-            reserveren_boek = input("1) Reserveer het boek \n0) Reserveer het boek niet")
-            start_keuze_gebruiker = input("6) Zoek opnieuw \n0) Terug naar menu")
-        else:
-            print("titel bestaat niet")
-            start_keuze_gebruiker = input("6) Zoek opnieuw \n0) Terug naar menu")
-        
