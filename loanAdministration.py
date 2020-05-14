ca552685418c36397633f995147436e14b362765
johnny090807 <33625576+johnny090807@users.noreply.github.com>
2020-05-14 16:19:04 +0200

    nice

--- loanAdministration.py
+++ loanAdministration.py
@@ -1,12 +1,10 @@
 import csv
 # from Person import Person
-# from Catalog import Catalog
 # from Book import Book
-# catalog = Catalog()
-# books = catalog.returnBooks()
 #Loanitem/loanadministration:
 #- Een loanid moet aan een persoonid gekoppeld kunnen worden, en er moet 1 boek in de loanitem af gaan
 #- Een loanid moet losgekoppeld kunnen worden van een persoonid, en er moet 1 boek bij komen bij loanitem
+
 
 class LoanAdministration:
     def __init__(self, persoonId, loanId):
